bef3c34e12fbfac252e1998b43e64f62f459147c
felexiusbry <bryanamalo12@gmail.com>
2025-02-13 11:59:41 +0700

    first commit

---
+++ PP12025_C_233040005/src/pertemuan2/Node.java
@@ -0,0 +1,13 @@
+package pertemuan2;
+
+public class Node {
+	private int nilai;
+	private node next;
+	
+	//inisialisasi Node
+	public Node(int nilai) {
+		this.nilai = nilai;
+	}
+	
+	// Setter & Getter
+}
